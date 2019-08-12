a777ec48380fe7421eee74fc5cd8498334e68043
Bright Phakamad <bright.sp@hotmail.com>
2019-08-12 07:34:50 -0500

    modified rgbToHex function for email

--- src/app/preview-email/preview-email.component.ts
+++ src/app/preview-email/preview-email.component.ts
@@ -33,10 +33,10 @@ export class PreviewEmailComponent implements DoCheck {
     }
 
     if (this.txtColor[0].color !== this.previousHeadColor) {
-      this.getHeadColor(this.txtColor[0].color);
+      this.setHeadcolor(this.txtColor[0].color);
     }
     if (this.txtColor[2].color !== this.previousParaColor) {
-      this.getParaColor(this.txtColor[2].color);
+      this.setParacolor(this.txtColor[2].color);
     }
 
     this.getHTML();
@@ -52,16 +52,14 @@ export class PreviewEmailComponent implements DoCheck {
     // tmp = $('div.preheader').parent().html();
     // tmp = $('div.preheader').parents('div#email').html();
     tmp = $('.email-template').html();
-    this.getHeadColor(this.txtColor[0].color);
-    this.getParaColor(this.txtColor[2].color);
+    this.setHeadcolor(this.txtColor[0].color);
+    this.setParacolor(this.txtColor[2].color);
 
     try {
 
-      if (tmp.includes('btn--')) {
-        const str = tmp.substring(tmp.search('btn--'), tmp.search('c-hero__action'));
-        const res = tmp.replace(str, 'btn--' + this.button + ' ');
-        tmp = res;
-        this.HTMLCode = tmp;
+      if ($('.headline').parent().html() !== undefined) {
+        tmp = this.rgbToHexHeadline();
+        tmp = this.rgbToHexPara(tmp);
       }
 
       /* Remove button code if not chosen  */
@@ -70,18 +68,17 @@ export class PreviewEmailComponent implements DoCheck {
         const str2 = tmp.substring(tmp.search('wht-btn') - 11, tmp.search('logo') - 11);
         tmp = tmp.replace(str1, '');
         tmp = tmp.replace(str2, '');
-        this.HTMLCode = tmp;
+
       } else if (this.button === 'default') {
         const str = tmp.substring(tmp.search('wht-btn') - 11, tmp.search('logo') - 11);
         tmp = tmp.replace(str, '');
-        this.HTMLCode = tmp;
+
       } else if (this.button === 'alternate') {
         const str = tmp.substring(tmp.search('blue-btn') - 11, tmp.search('wht-btn') - 11);
         tmp = tmp.replace(str, '');
-        this.HTMLCode = tmp;
+
       }
 
-      tmp = this.rgbToHex(tmp);
       this.HTMLCode = tmp;
       this.emailCode.emit(this.HTMLCode);
       insertEmail(this.HTMLCode);
@@ -90,17 +87,19 @@ export class PreviewEmailComponent implements DoCheck {
 
   }
 
-  /* Headline color */
-  getHeadColor(color) {
+  /* Set headline color */
+  setHeadcolor(color) {
     switch (color) {
       case 'blue':
         $('td').find('.headline').css('color', '#005da3'); // rgb(0, 93, 163)
         this.previousHeadColor = 'blue';
         break;
+
       case 'black':
         $('td').find('.headline').css('color', '#1e1e1e'); // rgb(30, 30, 30)
         this.previousHeadColor = 'black';
         break;
+
       case 'red':
         $('td').find('.headline').css('color', '#ed1a3b'); // rgb(237, 26, 59)
         this.previousHeadColor = 'red';
@@ -109,17 +108,19 @@ export class PreviewEmailComponent implements DoCheck {
 
   }
 
-  /* Paragraph color */
-  getParaColor(color) {
+  /* Set paragraph color */
+  setParacolor(color) {
     switch (color) {
       case 'blue':
         $('td').find('.p-medium').css('color', '#005da3');
         this.previousParaColor = 'blue';
         break;
+
       case 'black':
         $('td').find('.p-medium').css('color', '#1e1e1e');
         this.previousParaColor = 'black';
         break;
+
       case 'red':
         $('td').find('.p-medium').css('color', '#ed1a3b');
         this.previousParaColor = 'red';
@@ -127,39 +128,42 @@ export class PreviewEmailComponent implements DoCheck {
     }
   }
 
-  rgbToHex(output) {
-    let tmp: string;
+  rgbToHexHeadline() {
+    const str = $('.headline').parent().html();
     let res: string;
-    const hdTmp = output.substring( output.indexOf('headline') + 279, output.indexOf('headline') + 296 );
 
-    if (hdTmp === 'rgb(0, 93, 163); ') {
-      tmp = output.replace(hdTmp, '#005da3; ');
-      res = this.getHEXPara(tmp);
-      return res;
-    } else if (hdTmp === 'rgb(30, 30, 30); ') {
-      tmp = output.replace(hdTmp, '#1e1e1e; ');
-      res = this.getHEXPara(tmp);
-      return res;
-    } else if (hdTmp === 'rgb(237, 26, 59);') {
-      tmp = output.replace(hdTmp, '#ed1a3b;');
-      res = this.getHEXPara(tmp);
-      return res;
+    if ($('.headline').parent().html().includes('rgb(0, 93, 163)')) {
+      const tmp = $('.headline').parent().html().replace('rgb(0, 93, 163)', '#005da3');
+      res = $('.email-template').html().replace(str, tmp);
+
+    } else if ($('.headline').parent().html().includes('rgb(30, 30, 30)')) {
+      const tmp = $('.headline').parent().html().replace('rgb(30, 30, 30)', '#1e1e1e');
+      res = $('.email-template').html().replace(str, tmp);
+
+    } else if ($('.headline').parent().html().includes('rgb(237, 26, 59)')) {
+      const tmp = $('.headline').parent().html().replace('rgb(237, 26, 59)', '#ed1a3b');
+      res = $('.email-template').html().replace(str, tmp);
     }
+
+    return res;
+
   }
 
-  getHEXPara(output) {
+  rgbToHexPara(output) {
     let paraHex: string;
     const paraTmp = output.substring( output.indexOf('p-medium') + 132, output.indexOf('p-medium') + 149 );
 
     if (paraTmp === 'rgb(0, 93, 163); ') {
       paraHex = output.replace(paraTmp, '#005da3; ');
-      return paraHex;
+
     } else if (paraTmp === 'rgb(30, 30, 30); ') {
       paraHex = output.replace(paraTmp, '#1e1e1e; ');
-      return paraHex;
+
     } else if (paraTmp === 'rgb(237, 26, 59);') {
       paraHex = output.replace(paraTmp, '#ed1a3b;');
-      return paraHex;
     }
+
+    return paraHex;
   }
+
 }
